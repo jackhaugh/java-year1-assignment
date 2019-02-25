e784d22819d79f97841ae3f4071ea8c3169fbf63
Jack Haugh <jack.haugh@magairports.com>
2019-02-25 16:00:23 +0000

    LetterFrequency

---
+++ src/LetterFrequency.java
@@ -0,0 +1,15 @@
+
+public class LetterFrequency {
+	
+	private String input;
+	
+	public LetterFrequency(String stringInput) {
+		input = stringInput;
+	}
+	
+	public char[] inputStringToCharacters(String input) {
+		
+		char[] characterArray = input.toCharArray();
+	}
+
+}
